5dd175b025c887ae6f452f3171dd4195c63fed83
BhumikaYeole <yeolebhumika@gmail.com>
2026-07-12 10:33:49 +0530

    implementation of jwt auth

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import { PORT } from "./config/env.js"
 import connectToDatabase from "./database/mongodb.js"
 import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth_routes.js"
+import authorize from "./middleware/auth_middleware.js"
 
 const app = express()
 
@@ -9,6 +11,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended : false}))
 
+app.use("/api/auth",authRouter);
 
 app.get("/", (req,res) => {
     res.send("TransitOps running")
